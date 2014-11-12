ef3c4cd9e1472be97ee882cbb0ed5b1954b4b4dd
eric oberla <ejo@uchicago.edu>
2014-11-11 18:19:59 -0600

    c structure for data handling and packetizing

--- include/SuMo.h
+++ include/SuMo.h
@@ -1,3 +1,11 @@
+///////////////////////////////////////////////////////////////////
+/*
+definitions for LAPPD PSEC4 DAQ readout
+updated 31-10-2014
+author: eric oberla
+*/
+///////////////////////////////////////////////////////////////////
+
 #ifndef SUMO_H
 #define SUMO_H
 
@@ -5,15 +13,18 @@
 #include <stdio.h>
 #include "stdUSB.h"
 
-#define cc_buffersize 31
-#define ac_buffersize 8100
-#define all_ac_buffersize 24400
-#define AC_CHANNELS   30
-#define psec_buffersize 1536
-#define info_buffersize 16
-#define USB_READ_OFFSET 13
+#define cc_buffersize         31
+#define ac_buffersize         8100
+#define all_ac_buffersize     24400
+#define AC_CHANNELS           30
+#define psec_buffersize       1536
+#define infoBuffersize        16
+#define USB_READ_OFFSET       13
+#define psecSampleCells       256
+#define numChipsOnBoard       5
 
-#define WRAP_CONSTANT 90
+#define WRAP_CONSTANT         90
+#define numFrontBoards        4
 
 class SuMo{
 
@@ -43,7 +54,7 @@ class SuMo{
   int read_ACS(bool ENABLE_FILESAVE);
 
   int dump_data();
-  int get_AC_info(bool PRINT);
+  int get_AC_info(bool PRINT, int AC_adr);
   int generate_ped(bool ENABLE_FILESAVE);
   int make_count_to_voltage(void);
   int load_lut();
@@ -51,50 +62,57 @@ class SuMo{
   int scope_AC(int trig_mode, bool output_mode, int AC_adr);
   int log_data(const char* log_filename, unsigned int NUM_READS, int trig_mode, int acq_rate);
   int log_data_hd5(const char* log_filename, unsigned int NUM_READS, int trig_mode, 
-		   int acq_rate, int boards);
+		   int acq_rate);
 
   int check_active_boards(void);
+  int check_active_boards(int NUM);
 
   stdUSB usb;
  
  private:
-  unsigned int LAST_AC_INSTRUCT;
-  unsigned int LAST_LAST_AC_INSTRUCT;
-  unsigned int EVENT_COUNT;
-  unsigned long long int TIMESTAMP;
-  float VBIAS[6];
-  float RO_CNT[6];
-  float RO_TARGET_CNT[6];
-  float RO_DAC_VALUE[6];
-  int LAST_SAMPLING_BIN[6];
-  float TRIGGER_THRESHOLD[6];
-  bool SELF_TRIG_MODE[6];
-  bool TRIG_SIGN[6];
-  bool SAMPLING_SPEED[6];
+  static int compare ( const void * a, const void * b){
+    return(*(unsigned short*)a - *(unsigned short*)b);
+  }
+  int unwrap(int ASIC);
+  void unwrap_baseline(int *baseline, int ASIC);
 
-  //metadata from CC
+  /* metadata from CC */
+  unsigned int   CC_INFO[cc_buffersize];  
   unsigned int LAST_CC_INSTRUCT;
   unsigned long long CC_TIMESTAMP;
   unsigned int CC_EVENT_COUNT;
   unsigned int CC_BIN_COUNT;
   unsigned int CC_EVENT_NO;
-
-  static int compare ( const void * a, const void * b){
-    return(*(unsigned short*)a - *(unsigned short*)b);
-  }
-
-  int unwrap(int ASIC);
-  void unwrap_baseline(int *baseline, int ASIC);
+  bool DC_ACTIVE[numFrontBoards];
   
-  bool DC_ACTIVE[4];
-  unsigned short AC_RAW_DATA[5][psec_buffersize];
-  unsigned short AC_INFO[5][info_buffersize];
-  unsigned int   CC_INFO[cc_buffersize];
-  unsigned int PED_DATA[4][AC_CHANNELS][256];
-  float PED_STDEV[4][AC_CHANNELS][256];
-  float LUT[6][4096][AC_CHANNELS];
-  float oldLUT[6][4096][AC_CHANNELS];
-
+  /* calibration arrays */
+  unsigned int PED_DATA[numFrontBoards][AC_CHANNELS][psecSampleCells];
+  float PED_STDEV[numFrontBoards][AC_CHANNELS][psecSampleCells];
+  float LUT[numFrontBoards][4096][AC_CHANNELS];
+  float oldLUT[numFrontBoards][4096][AC_CHANNELS];
+   
+  /* waveform and meta-data arrays from ADC boards */
+  struct data_t{
+    unsigned int LAST_AC_INSTRUCT;
+    unsigned int LAST_LAST_AC_INSTRUCT;
+    unsigned int EVENT_COUNT;
+    unsigned long long int TIMESTAMP;
+    float VBIAS[numChipsOnBoard];
+    float RO_CNT[numChipsOnBoard];
+    float RO_TARGET_CNT[numChipsOnBoard];
+    float RO_DAC_VALUE[numChipsOnBoard];
+    int   LAST_SAMPLING_BIN[numChipsOnBoard];
+    float TRIGGER_THRESHOLD[numChipsOnBoard];
+    bool  SELF_TRIG_MODE[numChipsOnBoard];
+    bool  TRIG_SIGN[numChipsOnBoard];
+    bool  SAMPLING_SPEED[numChipsOnBoard];
+    int trigMode;
+    int trigSign;
+    
+    /* raw data saved here */
+    unsigned short AC_RAW_DATA[numChipsOnBoard][psec_buffersize];
+    unsigned short AC_INFO[numChipsOnBoard][infoBuffersize];
+  } acdcData [numFrontBoards];
+  
 };       
-
 #endif
