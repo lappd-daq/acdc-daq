67766b69afc7a49e4550450eca2883357a1536e5
wetstein-lab-1 <wetstein@iastate.edu>
2018-12-07 11:52:45 -0600

    switched linking type for SuMo

--- include/stdUSB.h
+++ include/stdUSB.h
@@ -47,8 +47,8 @@ private:
     struct usb_device *init(int num);
 
     /* USBFX2 device descriptions */
-    static const uint16_t USBFX2_VENDOR_ID = VID2; //0x090c;
-    static const uint16_t USBFX2_PRODUCT_ID = PID2; //0x1000;
+    static const uint16_t USBFX2_VENDOR_ID = VID1; //0x090c;
+    static const uint16_t USBFX2_PRODUCT_ID = PID1; //0x1000;
 
 protected:
     /* The handle to the usb device. Needed by write & read operations. */
